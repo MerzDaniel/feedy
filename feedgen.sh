669e9f0672c98cd4ade9010d464ec57b27915377
Daniel Merz <merzdanieldeveloper@gmail.com>
2022-08-23 01:06:49 +0200

    Add basic script

---
+++ feedgen.sh
@@ -0,0 +1,56 @@
+
+get_date() {
+  date +%FT%R:00Z
+}
+
+get_basic_feed() {
+  cat << EOF
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+<title>Title of your feed</title>
+<subtitle>Subtitle of your feed</subtitle>
+<link href="http://domain.com"/>
+EOF
+
+  echo "<updated>$(get_date)</updated>"
+  cat << EOF
+<author>
+<name>Your Name</name>
+<email>you@domain.com</email>
+</author>
+</feed>
+EOF
+}
+
+get_entry() {
+  cat << EOF
+<entry>
+<title>Your Title Here</title>
+<link href="http://domain.com/page/ "/>
+<id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
+EOF
+
+  echo "<updated>$(get_date)</updated>"
+  cat << EOF
+<summary>Short Summary</summary>
+</entry>
+EOF
+}
+
+feed=$1
+
+if [[ $feed != *.atom ]] ; then echo nope ; exit 1; fi
+
+if [ ! -f $feed ] ; then 
+  get_basic_feed > $feed
+fi
+
+# remove closing feed
+sed -i '$d' $feed
+
+# add entry
+get_entry >> $feed
+
+# close feed
+echo '</feed>' >> $feed
+
